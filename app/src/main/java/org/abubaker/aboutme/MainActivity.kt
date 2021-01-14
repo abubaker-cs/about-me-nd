631a0f8a773be2d81cbe347a90891ed3691f9a18
abubaker <mohammad_abubaker@yahoo.com>
2021-01-15 02:58:43 +0500

    Keyboard now hides when the DONE button is clicked.

--- app/src/main/java/org/abubaker/aboutme/MainActivity.kt
+++ app/src/main/java/org/abubaker/aboutme/MainActivity.kt
@@ -1,6 +1,13 @@
 package org.abubaker.aboutme
 
+import android.content.Context
+import android.hardware.input.InputManager
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +17,25 @@ class MainActivity : AppCompatActivity() {
 
         // Launch our activity view
         setContentView(R.layout.activity_main)
+
+        // Done - Actions
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard on pressing OK button
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
 }
